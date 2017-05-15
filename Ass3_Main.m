a2ecf63bc7fb136dbb790c3697725039e1276fd4
krassool <krassool@student.unimelb.edu.au>
2017-05-15 13:27:22 +1000

    add main

---
+++ Ass3_Main.m
@@ -0,0 +1,8 @@
+% William Page (587000) - Kevin Rassool (540733)   ;
+% Semester 2 2017 - University of Melbourne        ; Started:     15/5/17
+% MCEN90018 - Advanced Fluid Dynamics              ; Last Edited: 28/5/17
+% Assignment 3
+%
+% Main Script
+%% Import Data
+

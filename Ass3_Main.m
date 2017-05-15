ed92603646eeb90d3fbf98c784aef8b058869e60
willpage32 <willpage32@gmail.com>
2017-05-15 17:10:43 +1000

    energy spectra stuff in

--- Ass3_Main.m
+++ Ass3_Main.m
@@ -11,5 +11,14 @@ fid = fopen('MATLAB/Data/u_hf_ypos1.bin', 'r');
 data = fread(fid, '*double');
 
 %% See whats in the box today
-data(1:100)
-plot(data(1:100))
+
+G = fft(data);
+N = length(G);
+A = sqrt (4*(G./N).*(conj(G/N)) );
+
+up2nyq = 1:1:N/2+1;
+
+figure ; plot(up2nyq,A(up2nyq)) ; title('Energy>?')
+
+% data(1:100)
+% plot(data(1:100))
